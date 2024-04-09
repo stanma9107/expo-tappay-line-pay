130b5de1fa38e97bff7f62735c964172d3f68ee2
Stan <lol97554@gmail.com>
2024-04-09 19:09:57 +0800

    feat(plugin): setup environment for Android.

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,4 +1,8 @@
-import { withInfoPlist, ConfigPlugin } from "expo/config-plugins";
+import {
+  withInfoPlist,
+  ConfigPlugin,
+  withAndroidManifest,
+} from "expo/config-plugins";
 
 const withLSApplicationQueriesSchemes: ConfigPlugin = (config) => {
   config = withInfoPlist(config, (config) => {
@@ -13,4 +17,55 @@ const withLSApplicationQueriesSchemes: ConfigPlugin = (config) => {
   return config;
 };
 
-export default withLSApplicationQueriesSchemes;
+const withAllowBackupFalse: ConfigPlugin = (config) => {
+  return withAndroidManifest(config, (config) => {
+    console.log("Applying withAllowBackupFalse...");
+
+    if (
+      !config.modResults.manifest.application ||
+      !Array.isArray(config.modResults.manifest.application)
+    ) {
+      console.log(
+        "Unexpected format in AndroidManifest.xml. Make sure you have an <application> tag.",
+      );
+      return config;
+    }
+
+    for (const application of config.modResults.manifest.application) {
+      if (application && application["$"]) {
+        application["$"]["android:allowBackup"] = "false";
+        console.log("Set android:allowBackup to false.");
+      } else {
+        console.log("Skipped an application element.");
+      }
+    }
+
+    return config;
+  });
+};
+
+const withLinePayQueryScheme: ConfigPlugin = (config) => {
+  config = withAndroidManifest(config, (config) => {
+    console.log("Applying queries in manifest...");
+    config.modResults.manifest.queries.push({
+      package: [
+        {
+          $: {
+            "android:name": "jp.naver.line.android",
+          },
+        },
+      ],
+    });
+    return config;
+  });
+  return config;
+};
+
+const withConfig: ConfigPlugin = (config) => {
+  config = withLSApplicationQueriesSchemes(config);
+  config = withAllowBackupFalse(config);
+  config = withLinePayQueryScheme(config);
+  return config;
+};
+
+export default withConfig;
